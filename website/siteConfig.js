a20f2787bb05482802b7f7d6d2cda391675d2814
FINOS Administrator <37706051+finos-admin@users.noreply.github.com>
2019-02-12 19:43:54 +0100

    updating finos-deg repoUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -62,7 +62,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/finos-deg/finos-deg.github.io'
+  repoUrl: 'https://github.com/finos-deg/finos-deg'
 };
 
 module.exports = siteConfig;
